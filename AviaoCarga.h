a02f0f86929562b3c94a12fd51deb5ba610ad52e
Luan Oliveira <luanoliveira35@hotmail.com>
2015-11-05 20:35:08 -0300

    Adicionado os parametros do Aviao (Classe Base) ao contrutor AviaoCarga.

--- AviaoCarga.h
+++ AviaoCarga.h
@@ -3,7 +3,7 @@ class AviaoCarga: public Aviao{
 	
 	public:
 
-		AviaoCarga(bool = false, int = 0, int = 500 );
+		AviaoCarga(float = 1000.0, int = 700, int = 250 ,bool = false, int = 0, int = 500 );
 		AviaoCarga( const Aviao &);
 		void abrirPortadeCarga();
 		void adicionarCarga(int);
